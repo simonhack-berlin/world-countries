af371df4acfb6d33ca00875732247cfb790b1ae2
Simone Capuano <scapuano19@gmail.com>
2021-11-08 16:44:15 +0100

    implemented the search bar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,33 +5,69 @@ import './HomePage.css';
 const HomePage = () => {
 const [countries, setCountries] = useState([]);
 const [loading, setLoading] = useState(true);
+const [countryName, setCountryName] = useState("");
 
 useEffect(() => {
+    if (countryName) {
 axios
-.get('https://restcountries.com/v2/all')
+.get(`https://restcountries.com/v2/name/${countryName}?fullText=true`)
 .then((res) => {
     console.log(res.data);
-    setCountries(res.data);
+    setCountryName(res.data);
 })
 .then(() => setLoading(false))
 .catch((err) => {
     console.log(err);
 });
+    } else {
+        axios
+        .get('https://restcountries.com/v2/all')
+        .then((res) => {
+            console.log(res.data);
+            setCountries(res.data);
+        })
+        .then(() => setLoading(false))
+        .catch((err) => {
+            console.log(err);
+        });  
+    }
 }, [] );
 
+const contryOnChange = (event) => {
+    console.log(event.target.value);
+    setCountryName(event.target.value);
+};
+
 return ( 
+    <>
+    <form className="input-container">
+    <input 
+    className="search-bar"
+    type="text" 
+    placeholder="search for a country..." 
+    onChange={contryOnChange}
+    />
+    </form>
     <div className="countries-container">
-{loading ? <div>Loading...</div> : (countries.map(country => 
-<div key={country.name} className="country-card">
+{loading ? <div>Loading...</div> :  (
+    countries.filter((country) =>{
+       if (country.name.toLowerCase().includes(countryName.toLowerCase())) {
+           return country
+       }
+    })
+    .map(country =>
+        <div className="country-card">
 <img src={country.flag} alt={country.name} className="flag-img" />
 <h1>{country.name}</h1>
 <p><b>Population:</b> {country.population}</p>
 <p><b>Region:</b> {country.region}</p>
 <p><b>Capital:</b> {country.capital}</p>
 </div>
-)
-)  }
+    )
+    )
+ }
     </div>
+    </>
 )
 
 };
