06b5739dc33b9a9fe495397085759d38fbb9f2c3
Simone Capuano <scapuano19@gmail.com>
2021-11-16 17:36:55 +0100

    get single country description

--- src/Pages/Country/Country.js
+++ src/Pages/Country/Country.js
@@ -12,7 +12,7 @@ const Country = () => {
     useEffect(() => {
   
       axios
-      .get('https://restcountries.com/v3.1/all')
+      .get('https://restcountries.com/v2/all')
       .then((res) => {
           console.log(res.data);
           setCountries(res.data);
@@ -24,17 +24,19 @@ const Country = () => {
   
   }, [] );
 
-const MY_API = 'AIzaSyCc3zoz5TZaG3w2oF7IeR-fhxNXi8uywNk';
+const My_Key = 'AIzaSyCc3zoz5TZaG3w2oF7IeR-fhxNXi8uywNk';
 
 return (
     <>
     {loading ? <div>Loading...</div> : (
-        countries.filter(country => country.name.common === name).map((country, index) => (
+        countries.filter(country => country.name === name).map((country, index) => (
         <div key={index}>
-            <h1>{country.name.common}</h1>
+            {/* <h1>{country.altSpellings[1]}</h1> */}
+            <p><b>{country.name}</b> is a country in {country.subregion}, and the official language is {country.languages[0].name}.</p>
+            <p>With <b>{country.capital}</b> as capital, the country covers a total area of {country.area} km2 and has {country.population} inhabitants.</p>
             
-            <div id="wrap">
-      <iframe id="frame" frameBorder="0" width="100%" height="700px" src={`https://www.google.com/maps/embed/v1/place?key=${MY_API}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}>
+    <div id="map-container">
+      <iframe id="frame" frameBorder="0" width="100%" height="500px" src={`https://www.google.com/maps/embed/v1/place?key=${My_Key}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}>
       </iframe>
     </div>
     
