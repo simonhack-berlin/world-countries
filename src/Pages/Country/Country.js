22eed9f28c8ac27d69744093d5a47d70dfee0e0d
Simone Capuano <scapuano19@gmail.com>
2022-11-06 09:04:42 +0100

    style and text in country page

--- src/Pages/Country/Country.js
+++ src/Pages/Country/Country.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router';
 import './Country.css';
+import Button from '../../Components/Button';
 
 
 const Country = () => {
@@ -40,28 +41,31 @@ let population = getCountry.map(country => country.population === 0 ? 'no' : cou
 let capital = getCountry.map(country => !country.capital ? null : country.capital.length === 2 ? (country.capital.join(' and ')) : country.capital.length > 2 ? country.capital.slice(0, -1).join(', ') + ', and '+ country.capital.slice(-1) : country.capital);
 
 return (
-    <>
-    <button className="back-btn" onClick={goBack}>&#8592; Back</button>
-    {loading ? <div style={{textAlign: 'center', marginTop: '30px'}}><h1>Loading...</h1></div> : (
+    <section className="country">
+      <Button onClick={goBack}>&#8592; Back</Button>
+      {loading ? <div style={{textAlign: 'center', marginTop: '30px'}}><h1>Loading...</h1></div> : (
         getCountry.map((country, index) => (
-        <div key={index} className="description-container">
-        <div className="description-txt">
-        {country.name.official === country.name.common ? <h1>Learn more about {country.name.common} {country.flag}</h1> : <h1>Learn more about {country.name.official} ({country.name.common}) {country.flag}</h1>}
-        {country.subregion ? <p><b>{country.name.common}</b> is a country in {country.subregion}{languages}.</p> : <p><b>{country.name.common}</b> is a country in {country.region}{languages}.</p>}
-        {country.capital ? <p>With <b>{capital}</b> as capital, the country covers a total area of {country.area} km2 and has {population} inhabitants.</p> : <p>The country covers a total area of {country.area} km2 and has {population} inhabitants.</p>}
-        </div>    
-    <div id="map-container">
-      <iframe id="frame" frameBorder="0" width="100%" src={`https://www.google.com/maps/embed/v1/place?key=${My_Key}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}>
-      </iframe>
-    </div>
-    
-        </div>
-    ))
-    )
-        }
-    
-    </>
-)
+          <div key={index} className="description-container">
+
+            <div className="img-container">
+              <img src={country.flags.png} alt={country.name.common} />
+            </div>
+            
+            <div className="description-txt">
+              {country.name.official === country.name.common ? <h1>{country.name.common}</h1> : <h1>{country.name.official} ({country.name.common})</h1>}
+              {country.subregion ? <p>is a country in {country.subregion}{languages}.</p> : <p>is a country in {country.region}{languages}.</p>}
+              {country.capital ? <p>With <b>{capital}</b> as capital, the country covers a total area of {country.area} km2 and has {population} inhabitants.</p> : <p>The country covers a total area of {country.area} km2 and has {population} inhabitants.</p>}
+            </div>
+
+            <div id="map-container">
+              <iframe title="location" id="frame" frameBorder="0" width="100%" src={`https://www.google.com/maps/embed/v1/place?key=${My_Key}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}></iframe>
+            </div>      
+
+          </div>
+        ))
+      )}    
+    </section>
+  )
 }
 
 export default Country;
