1f827ebbbb416a9a11c7a5a7a11615bb45efce46
Simone Capuano <scapuano19@gmail.com>
2024-03-26 16:47:48 +0100

    style refactoring

--- src/Pages/Country/Country.js
+++ src/Pages/Country/Country.js
@@ -1,71 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router';
-import './Country.css';
-import Button from '../../Components/Button';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router";
+import "./Country.css";
+import Button from "../../Components/Button";
 
 const Country = () => {
-    const {name} = useParams();
-    const [countries, setCountries] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+  const { name } = useParams();
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    const goBack = () => {
-        navigate('/');
-      };
+  const goBack = () => {
+    navigate("/");
+  };
 
-    useEffect(() => {
-  
-      axios
-      .get('https://restcountries.com/v3.1/all')
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
       .then((res) => {
         //   console.log(res.data);
-          setCountries(res.data);
+        setCountries(res.data);
       })
       .then(() => setLoading(false))
       .catch((err) => {
-          console.log(err);
-      });  
-  
-  }, [] );
+        console.log(err);
+      });
+  }, []);
 
-const My_Key = 'AIzaSyCc3zoz5TZaG3w2oF7IeR-fhxNXi8uywNk';
+  const My_Key = "AIzaSyCc3zoz5TZaG3w2oF7IeR-fhxNXi8uywNk";
 
-const getCountry = countries.filter(country => country.name.common === name);
+  const getCountry = countries.filter(
+    (country) => country.name.common === name
+  );
 
-let languages = getCountry.map(country => !country.languages ? null : Object.keys(country.languages).length === 2 ? (', and the official languages are ' + Object.values(country.languages).join(' and ')) : Object.keys(country.languages).length > 2 ? (', and the official languages are ' + Object.values(country.languages).slice(0, -1).join(', ') + ', and '+ Object.values(country.languages).slice(-1)) : (', and the official language is ' + Object.values(country.languages).toString()) );
+  let languages = getCountry.map((country) =>
+    !country.languages
+      ? null
+      : Object.keys(country.languages).length === 2
+      ? ", and the official languages are " +
+        Object.values(country.languages).join(" and ")
+      : Object.keys(country.languages).length > 2
+      ? ", and the official languages are " +
+        Object.values(country.languages).slice(0, -1).join(", ") +
+        ", and " +
+        Object.values(country.languages).slice(-1)
+      : ", and the official language is " +
+        Object.values(country.languages).toString()
+  );
 
-let population = getCountry.map(country => country.population === 0 ? 'no' : country.population);
+  let population = getCountry.map((country) =>
+    country.population === 0 ? "no" : country.population
+  );
 
-let capital = getCountry.map(country => !country.capital ? null : country.capital.length === 2 ? (country.capital.join(' and ')) : country.capital.length > 2 ? country.capital.slice(0, -1).join(', ') + ', and '+ country.capital.slice(-1) : country.capital);
+  let capital = getCountry.map((country) =>
+    !country.capital
+      ? null
+      : country.capital.length === 2
+      ? country.capital.join(" and ")
+      : country.capital.length > 2
+      ? country.capital.slice(0, -1).join(", ") +
+        ", and " +
+        country.capital.slice(-1)
+      : country.capital
+  );
 
-return (
+  return (
     <section className="country">
       <Button onClick={goBack}>&#8592; Back</Button>
-      {loading ? <div style={{textAlign: 'center', marginTop: '30px'}}><h1>Loading...</h1></div> : (
+      {loading ? (
+        <div style={{ textAlign: "center", marginTop: "30px" }}>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
         getCountry.map((country, index) => (
           <div key={index} className="description-container">
-
             <div className="img-container">
               <img src={country.flags.png} alt={country.name.common} />
             </div>
-            
+
             <div className="description-txt">
-              {country.name.official === country.name.common ? <h1>{country.name.common}</h1> : <h1>{country.name.official} ({country.name.common})</h1>}
-              {country.subregion ? <p>is a country in {country.subregion}{languages}.</p> : <p>is a country in {country.region}{languages}.</p>}
-              {country.capital ? <p>With <b>{capital}</b> as capital, the country covers a total area of {country.area} km2 and has {population} inhabitants.</p> : <p>The country covers a total area of {country.area} km2 and has {population} inhabitants.</p>}
+              {country.name.official === country.name.common ? (
+                <h1>{country.name.common}</h1>
+              ) : (
+                <h1>
+                  {country.name.official} ({country.name.common})
+                </h1>
+              )}
+              {country.subregion ? (
+                <p>
+                  is a country in {country.subregion}
+                  {languages}.
+                </p>
+              ) : (
+                <p>
+                  is a country in {country.region}
+                  {languages}.
+                </p>
+              )}
+              {country.capital ? (
+                <p>
+                  With <b>{capital}</b> as capital, the country covers a total
+                  area of {country.area} km2 and has {population} inhabitants.
+                </p>
+              ) : (
+                <p>
+                  The country covers a total area of {country.area} km2 and has{" "}
+                  {population} inhabitants.
+                </p>
+              )}
             </div>
 
             <div id="map-container">
-              <iframe title="location" id="frame" frameBorder="0" width="100%" src={`https://www.google.com/maps/embed/v1/place?key=${My_Key}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}></iframe>
-            </div>      
-
+              <iframe
+                title="location"
+                id="frame"
+                frameBorder="0"
+                width="100%"
+                src={`https://www.google.com/maps/embed/v1/place?key=${My_Key}&q=${country.latlng[0]},${country.latlng[1]}&zoom=6`}
+              ></iframe>
+            </div>
           </div>
         ))
-      )}    
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default Country;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,129 +1,177 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './HomePage.css';
-import Button from '../../Components/Button';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./HomePage.css";
+import Button from "../../Components/Button";
 
 const HomePage = () => {
-const [countries, setCountries] = useState([]);
-const [loading, setLoading] = useState(true);
-const [countryName, setCountryName] = useState("");
-const [region, setRegion] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [countryName, setCountryName] = useState("");
+  const [region, setRegion] = useState("");
 
-const API = 'https://restcountries.com/v3.1/all';
+  const API = "https://restcountries.com/v3.1/all";
 
-useEffect(() => {
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((res) => {
+        // console.log(res.data);
+        setCountries(res.data);
+      })
+      .then(() => setLoading(false))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-        axios
-        .get(API)
-        .then((res) => {
-            // console.log(res.data);
-            setCountries(res.data);
-        })
-        .then(() => setLoading(false))
-        .catch((err) => {
-            console.log(err);
-        });  
-    
-}, [] );
-
-const contryNameOnChange = (event) => {
+  const contryNameOnChange = (event) => {
     //console.log(event.target.value);
     setCountryName(event.target.value);
-};
+  };
 
-const contryRegionOnChange = (event) => {
-   // console.log(event.target.value);
+  const contryRegionOnChange = (event) => {
+    // console.log(event.target.value);
     setRegion(event.target.value);
-};
+  };
 
-const filteredByName =  countries.filter((country) =>{
+  const filteredByName = countries.filter((country) => {
     if (country.name.common.toLowerCase().includes(countryName.toLowerCase())) {
-        return country;
+      return country;
     }
- });
+  });
 
- const filteredByRegion = countries.filter((country) =>{
-     if (country.region.toLowerCase().includes(region.toLowerCase())) {
-         return country;
-     }
- });
+  const filteredByRegion = countries.filter((country) => {
+    if (country.region.toLowerCase().includes(region.toLowerCase())) {
+      return country;
+    }
+  });
 
-return ( 
+  return (
     <>
-    <form className="input-container">
-    <input 
-    className="search-bar"
-    type="text" 
-    placeholder="search for a country..." 
-    onChange={contryNameOnChange}
-    />
-    <select className="select-container" onChange={contryRegionOnChange}>
-        <option value="">Filter by region</option>
-        <option value="Africa">Africa</option>
-        <option value="America">America</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceania">Oceania</option>
-    </select>
-    </form>
-    <div className="countries-container">
-{loading ? <div style={{textAlign: 'center'}}><h1>Loading...</h1></div> : countryName ? (
-    filteredByName
-    .map(country =>
-    
-<div key={country.name.common} className="country-card">
-    <img src={country.flags.png} alt={country.name.common} className="flag-img" />
-    <div className="country-name">
-        <h1>{country.name.common}</h1>
-    </div>
-    <div className="country-details">
-        <p><b>Region:</b> {country.region}</p>
-        {country.capital ? <p><b>Capital:</b> {country.capital.join(', ')}</p> : <p><b>Capital:</b> No capital</p>}
-        <p><b>{country.independent ? 'Independent' : 'Not independent' }</b></p>
-    </div>
-    <Button link={`/${country.name.common}`}>More Details</Button>
-</div>
-  
-    )
-    ) : region ? (
-        filteredByRegion
-        .map(country =>
-
-<div key={country.name.common} className="country-card">
-    <img src={country.flags.png} alt={country.name.common} className="flag-img" />
-    <div className="country-name">
-        <h1>{country.name.common}</h1>
-    </div>
-    <div className="country-details">
-        <p><b>Region:</b> {country.region}</p>
-        {country.capital ? <p><b>Capital:</b> {country.capital.join(', ')}</p> : <p><b>Capital:</b> No capital</p>}
-        <p><b>{country.independent ? 'Independent' : 'Not independent' }</b></p>
-    </div>
-    <Button link={`/${country.name.common}`}>More Details</Button>
-</div>
-
-    )
-    ) : countries
-    .map(country =>
-
-<div key={country.name.common} className="country-card">
-    <img src={country.flags.png} alt={country.name.common} className="flag-img" />
-    <div className="country-name">
-        <h1>{country.name.common}</h1>
-    </div>
-    <div className="country-details">
-        <p><b>Region:</b> {country.region}</p>
-        {country.capital ? <p><b>Capital:</b> {country.capital.join(', ')}</p> : <p><b>Capital:</b> No capital</p>}
-        <p><b>{country.independent ? 'Independent' : 'Not independent' }</b></p>
-    </div>
-    <Button link={`/${country.name.common}`}>More Details</Button>
-</div>
-
-    )
- }
-    </div>
+      <form className="input-container">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search for a country..."
+          onChange={contryNameOnChange}
+        />
+        <select className="select-container" onChange={contryRegionOnChange}>
+          <option value="">--Filter by region--</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </form>
+      <div className="countries-container">
+        {loading ? (
+          <div style={{ textAlign: "center" }}>
+            <h1>Loading...</h1>
+          </div>
+        ) : countryName ? (
+          filteredByName.map((country) => (
+            <div key={country.name.common} className="country-card">
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                className="flag-img"
+              />
+              <div className="country-name">
+                <h1>{country.name.common}</h1>
+              </div>
+              <div className="country-details">
+                <p>
+                  <b>Region:</b> {country.region}
+                </p>
+                {country.capital ? (
+                  <p>
+                    <b>Capital:</b> {country.capital.join(", ")}
+                  </p>
+                ) : (
+                  <p>
+                    <b>Capital:</b> No capital
+                  </p>
+                )}
+                <p>
+                  <b>
+                    {country.independent ? "Independent" : "Not independent"}
+                  </b>
+                </p>
+              </div>
+              <Button link={`/${country.name.common}`}>More Details</Button>
+            </div>
+          ))
+        ) : region ? (
+          filteredByRegion.map((country) => (
+            <div key={country.name.common} className="country-card">
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                className="flag-img"
+              />
+              <div className="country-name">
+                <h1>{country.name.common}</h1>
+              </div>
+              <div className="country-details">
+                <p>
+                  <b>Region:</b> {country.region}
+                </p>
+                {country.capital ? (
+                  <p>
+                    <b>Capital:</b> {country.capital.join(", ")}
+                  </p>
+                ) : (
+                  <p>
+                    <b>Capital:</b> No capital
+                  </p>
+                )}
+                <p>
+                  <b>
+                    {country.independent ? "Independent" : "Not independent"}
+                  </b>
+                </p>
+              </div>
+              <Button link={`/${country.name.common}`}>More Details</Button>
+            </div>
+          ))
+        ) : (
+          countries.map((country) => (
+            <div key={country.name.common} className="country-card">
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                className="flag-img"
+              />
+              <div className="country-name">
+                <h1>{country.name.common}</h1>
+              </div>
+              <div className="country-details">
+                <p>
+                  <b>Region:</b> {country.region}
+                </p>
+                {country.capital ? (
+                  <p>
+                    <b>Capital:</b> {country.capital.join(", ")}
+                  </p>
+                ) : (
+                  <p>
+                    <b>Capital:</b> No capital
+                  </p>
+                )}
+                <p>
+                  <b>
+                    {country.independent ? "Independent" : "Not independent"}
+                  </b>
+                </p>
+              </div>
+              <Button link={`/${country.name.common}`}>More Details</Button>
+            </div>
+          ))
+        )}
+      </div>
     </>
-)
-}
+  );
+};
 
 export default HomePage;
